020b4cfa755d1306306419d8aa7ce763fa4b01e4
thakurshaurya <thakurshaurya2007@gmail.com>
2026-01-29 16:27:48 +0530

    Added Cart changes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
-function cartquantity(){
-    
+const cartBtn = document.querySelector("#Cartbtn");
+const cartQnt = document.querySelector(".cart-count");
+
+const CART_KEY = "cartCount";
+
+// Load cart count on EVERY page
+let val = Number(localStorage.getItem(CART_KEY)) || 0;
+
+if (cartQnt) {
+  cartQnt.innerText = val;
 }
+
+// Only attach click handler if button exists
+if (cartBtn) {
+  cartBtn.addEventListener("click", () => {
+    val += 1;
+    localStorage.setItem(CART_KEY, val);
+
+    if (cartQnt) {
+      cartQnt.innerText = val;
+    }
+  });
+}
+
